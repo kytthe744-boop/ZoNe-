98bc4c1505293668470a15fca25e6cf6e783dc8c
kytthe744-boop <kytthe744@gmail.com>
2025-12-29 00:44:09 +0530

    Implement overlay permission check in MainActivity

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.overlayapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        } else {
+            startService(new Intent(this, OverlayService.class));
+            finish();
+        }
+    }
+                              }
